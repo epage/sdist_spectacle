676e231a48959f6b761e33175a348dd6f288457b
Ed Page <eopage@byu.net>
2010-12-24 13:06:23 -0600

    First stage of creating sdist_spectacle

--- example_setup.py
+++ example_setup.py
@@ -1,29 +1,29 @@
-#
-# example_setup.py
-#
-# Example use of 'sdist_maemo' command.
-#
-#  Author: khertan@khertan.net
-#
-#    Date: 1 October 2010
-#
-# License: GPLv3
-#
+"""
+example_setup.py
 
-from sdist_maemo import sdist_maemo as _sdist_maemo
+Example use of 'sdist_spectacle' command.
+
+Author: eopage@byu.net
+
+24 December 2010
+
+License: GPLv3
+"""
+
+
+from sdist_spectacle import sdist_spectacle
 from distutils.core import setup
 
-setup(name="example",
-      scripts=['example'],
-      version='0.0.1',
-      maintainer="Khertan",
-      maintainer_email="khertan@khertan.net",
-      description="An example.",
-      long_description=\
-         "An example packed with sdist_maemo--the pure Python packager!",
-      data_files = [('share/applications/hildon', ['example.desktop'])],
-      cmdclass={'sdist_maemo': _sdist_maemo})
 
-
-            
+setup(
+    name="example",
+    scripts=['example'],
+    version='0.0.1',
+    maintainer="Ed Page",
+    maintainer_email="eopage@byu.net",
+    description="An example.",
+    long_description="An example packed with sdist_spectacle",
+    data_files = [('share/applications/hildon', ['example.desktop'])],
+    cmdclass={'sdist_spectacle': sdist_spectacle}
+)
 
